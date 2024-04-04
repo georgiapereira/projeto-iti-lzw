0ef304fa4ad102bc46edf2a1d5531482911bd552
Georgia Pereira <georgiapereirab@gmail.com>
2024-04-03 21:40:09 -0300

    ler do arquivo

--- projeto.py
+++ projeto.py
@@ -1,19 +1,14 @@
+import sys, os
+
 from bitarray import bitarray, frozenbitarray
-from bitarray.util import int2ba, ba2int
+from bitarray.util import int2ba, ba2int, strip
 
 
-def initialize_dictionary():
-    #return {bytes([i]): i for i in range(256)}
-    return {
-        b'A' : frozenbitarray('000'), 
-        b'B' : frozenbitarray('001'),
-        b'C' : frozenbitarray('010'),
-        b'D' : frozenbitarray('011'),
-        b'R' : frozenbitarray('100')
-    }
+def initialize_dictionary_encode():
+    return {bytes([i]): frozenbitarray(buffer=bytes([i])) for i in range(256)}
 
 def encode(data, dictionary):
-    b = 3
+    b = 8
     buffer = b'' # ultima frase encontrada no dicionario
     result = bitarray()
     for symbol in data: #
@@ -44,16 +39,54 @@ def encode(data, dictionary):
     return result
 
 def lzw_compress(data, static_dictionary=False):
-    dictionary = initialize_dictionary()
+    dictionary = initialize_dictionary_encode()
     #print("Dicionário inicial:", dictionary)
     compressed_data = encode(data, dictionary)
-    print(dictionary)
+    #print(dictionary)
     return compressed_data
 
+""" def initialize_dictionary_decode():
+    return {frozenbitarray(buffer=bytes([i])): bytes([i]) for i in range(256)}
 
-# Exemplo de uso:
-original_data = b'ABRACADABRA'
+def decode(data, dictionary):
+    b = 8
+    buffer = b'' # 
+    result = b''
+    while len(data) > 0: #
+        symbol = strip(data[:b], mode="left")
+        data = data[b:]
+        
+        result = result + dictionary[symbol]
+        if buffer:
+            dictionary[frozenbitarray(int2ba(len(dictionary))).to01()] = buffer + dictionary[symbol]
+
+        if 2 ** b < len(dictionary):
+            b = b + 1
+
+        buffer = dictionary[symbol] 
+    
+    return result
+
+def lzw_decompress(data, static_dictionary=False):
+    dictionary = initialize_dictionary_decode()
+    decompressed_data = decode(data, dictionary)
+    return decompressed_data
+ """
+
+file = sys.argv[1]
+fin = open(file, "rb")
+filename = os.path.splitext(os.path.basename(file))[0]
+
+original_data = fin.read()
+fin.close()
+
 compressed_data = lzw_compress(original_data, static_dictionary=True)
 print("Dados Comprimidos:", compressed_data)
-with open("output.bin", 'wb') as output:
+with open(f"{filename}.bin", 'wb') as output:
     compressed_data.tofile(output)
+
+#compressed_data = bitarray().fromfile(filename+".bin")
+#decompressed_data = lzw_decompress(compressed_data)
+#print("Dados descomprimidos: ", decompressed_data)
+
+
