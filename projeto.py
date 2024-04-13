38714c7cfcf92676393c9a56c89609f282b5cca0
Georgia Pereira <georgiapereirab@gmail.com>
2024-04-13 18:57:26 -0300

    compressão rc

--- projeto.py
+++ projeto.py
@@ -1,6 +1,7 @@
 import sys, os
 import math
 import time
+import matplotlib.pyplot as plt
 
 from bitarray import bitarray, frozenbitarray
 from bitarray.util import int2ba, ba2int, strip
@@ -26,9 +27,18 @@ def initialize_dictionary_encode():
 def encode(data, dictionary, file, p, static_dictionary, rc):
     n_symbol = 0
     n_bits = 0
+    tam_result = 0
+    tam_input = 0
+    graph_tam_symbol_x = []
+    graph_tam_symbol_y = []
+    cont_100_mais = False
+    posicao_100 = 0
+    flag_estatico_por_rc = False
+    posicao_delta = 0
     dict_cont_s = {}
     bits_to_write = 64
     b = 8
+    cont = 0
     buffer = b'' # ultima frase encontrada no dicionario
     result = bitarray()
     with open(file, 'wb') as write_file:
@@ -52,17 +62,47 @@ def encode(data, dictionary, file, p, static_dictionary, rc):
                     result = result + bitarray(b - tamanho_buffer)
 
                 result = result + dictionary[buffer]
+                tam_result += b
+                tam_input += len(buffer)
+                graph_tam_symbol_x.append(tam_input)
+                graph_tam_symbol_y.append(tam_result / tam_input)
+
                 #print('Bits por simbolo:', dictionary[buffer])
                 #print(result, "int: ", ba2int(dictionary[buffer]), "b:", b, "simbolo: ", buffer, "\n")
                 tamanho_dict = len(dictionary)
-                if tamanho_dict < p:
+                
+                if(rc and tamanho_dict >= p and not cont_100_mais and not flag_estatico_por_rc):
+                    posicao_100 = len(graph_tam_symbol_y)
+                    cont_100_mais = True
+                
+                if tamanho_dict < p or (cont_100_mais and not flag_estatico_por_rc):
                     dictionary[new_buffer] = frozenbitarray(int2ba(tamanho_dict))
                     newTamanho = len(dictionary[new_buffer])
                     if(newTamanho > b):
                         b = newTamanho
-                elif rc:
-                    print('TO-DO RC')
-                elif not static_dictionary:
+                    if(cont_100_mais and len(graph_tam_symbol_y) == posicao_100+100):
+                        cont_100_mais = False
+                        flag_estatico_por_rc = True
+
+                elif rc and (posicao_delta == 0 or posicao_delta+100 == len(graph_tam_symbol_y)): 
+                    delta_l =  graph_tam_symbol_y[len(graph_tam_symbol_y)-1] - graph_tam_symbol_y[len(graph_tam_symbol_y)-200]
+                   
+                    if(delta_l >= 1): #rc decrescente
+                        print(delta_l)
+                        flag_estatico_por_rc = False
+                        cont_100_mais = False
+                        b = 9
+                        dictionary = initialize_dictionary_encode()
+                        tamanho_dict = len(dictionary)
+                        dictionary[new_buffer] = frozenbitarray(int2ba(tamanho_dict))
+                        posicao_delta = 0
+
+                    else:
+                        posicao_delta = len(graph_tam_symbol_y) #480
+                        flag_estatico_por_rc = True
+                        cont_100_mais = True
+                    
+                elif not rc and not static_dictionary:
                     b = 9
                     dictionary = initialize_dictionary_encode()
                     tamanho_dict = len(dictionary)
@@ -86,12 +126,25 @@ def encode(data, dictionary, file, p, static_dictionary, rc):
             if(tamanho < b):
                 result = result + bitarray(b - tamanho)
             result = result + dictionary[buffer]
+            tam_result += b
+            tam_input += len(buffer)
+            graph_tam_symbol_x.append(tam_input)
+            graph_tam_symbol_y.append(tam_result / tam_input)
             #print('Simbolos: ', result)
             n_bits += len(result)
             write_file.write(result)
             #print(result, "int: ", ba2int(dictionary[buffer]), "b:", b, "simbolo: ", buffer, "\n")
         print ("Comprimento médio:", calc_comprimento_medio(n_bits,n_symbol))
         print ("Entropia: ", cal_entropia(dict_cont_s,n_symbol))
+        # Criando o gráfico
+        plt.plot(graph_tam_symbol_x, graph_tam_symbol_y)
+        plt.xscale('log')
+        # Adicionando rótulos e título
+        plt.xlabel('Eixo X')
+        plt.ylabel('Eixo Y')
+        plt.title('Gráfico de Linha Simples')
+        plt.show()
+
 
 def lzw_compress(data, file, p, static_dictionary=False, rc=False):
     dictionary = initialize_dictionary_encode()
@@ -107,38 +160,72 @@ def decode(data, dictionary, file, p, static_dictionary, rc):
     b = 8
     buffer = b'' # 
     result = b''
+    tam_result = 0
+    tam_input = 0
+    graph_tam_symbol_x = []
+    graph_tam_symbol_y = []
+    cont_100_mais = False
+    posicao_100 = 0
+    flag_estatico_por_rc = False
+    posicao_delta = 0
     cur_index = 0
+    cont = 0
     with open(file, 'wb') as write_file:
         while True: #
             dict_len = len(dictionary)
-            if not static_dictionary and dict_len == p:
+
+            if(rc and dict_len >= p and not cont_100_mais and not flag_estatico_por_rc):
+                posicao_100 = len(graph_tam_symbol_y)
+                cont_100_mais = True
+
+            aumenta_dicionario = dict_len < p or (cont_100_mais and not flag_estatico_por_rc)
+            mede_delta_l = not aumenta_dicionario and (rc and (posicao_delta == 0 or posicao_delta+100 == len(graph_tam_symbol_y)))
+            reseta_rc = mede_delta_l and graph_tam_symbol_y[len(graph_tam_symbol_y)-1] - graph_tam_symbol_y[len(graph_tam_symbol_y)-200] > 1
+            reseta_dicionario = not static_dictionary and dict_len >=p and (not rc or reseta_rc)
+
+            if reseta_dicionario:
                 b = 9
             symbol = strip(data[cur_index:b+cur_index], mode="left") #2
-            #symbol = data[:b] #2
-            #if(len(symbol) == 0): symbol = bitarray('0')
-            #print(data[:b], symbol, b)
-            #data = data[b:] #eof
             cur_index = cur_index + b
+            tam_result += b
 
             if len(symbol) < 8:
                 symbol = bitarray(8 - len(symbol)) + symbol
 
             frozen_symbol = frozenbitarray(symbol)
 
-            dict_len = len(dictionary)
-
-            if dict_len < p:
+            if aumenta_dicionario:
                 if buffer:
                     if frozen_symbol not in dictionary:
                         dictionary[frozenbitarray(int2ba(dict_len))] = buffer + bytes([buffer[0]]) 
                     else:
                         dictionary[frozenbitarray(int2ba(dict_len))] = buffer + bytes([dictionary[frozen_symbol][0]]) 
 
-                if 2 ** b <= len(dictionary) and 2 ** b < p:
+                if 2 ** b <= len(dictionary) and (not static_dictionary or 2 ** b < p):
                     b = b + 1 #6
-            elif rc:
-                print('TO-DO RC')
-            elif not static_dictionary:
+                if(cont_100_mais and len(graph_tam_symbol_y) == posicao_100+100):
+                        cont_100_mais = False
+                        flag_estatico_por_rc = True
+            elif mede_delta_l: 
+                    delta_l = graph_tam_symbol_y[len(graph_tam_symbol_y)-1] - graph_tam_symbol_y[len(graph_tam_symbol_y)-200]
+
+                    if(delta_l > 1): #rc decrescente
+                        print(delta_l)
+                        flag_estatico_por_rc = False
+                        cont_100_mais = False
+                        dictionary = initialize_dictionary_decode()
+                        if buffer:
+                            dict_len = len(dictionary)
+                            if frozen_symbol not in dictionary:
+                                dictionary[frozenbitarray(int2ba(dict_len))] = buffer + bytes([buffer[0]]) 
+                            else:
+                                dictionary[frozenbitarray(int2ba(dict_len))] = buffer + bytes([dictionary[frozen_symbol][0]]) 
+                        posicao_delta = 0
+                    else:
+                        posicao_delta = len(graph_tam_symbol_y) #480
+                        flag_estatico_por_rc = True
+                        cont_100_mais = True
+            elif not rc and not static_dictionary:
                 dictionary = initialize_dictionary_decode()
                 if buffer:
                     dict_len = len(dictionary)
@@ -151,6 +238,9 @@ def decode(data, dictionary, file, p, static_dictionary, rc):
             #print(result, "int: ", ba2int(symbol), "b:", b, "simbolo: ", dictionary[frozenbitarray(symbol)], "\n")
            
             result = result + dictionary[frozen_symbol] #a,a,b,ab,aba, aa
+            tam_input += len(dictionary[frozen_symbol] )
+            graph_tam_symbol_x.append(tam_input)
+            graph_tam_symbol_y.append(tam_result / tam_input)
 
             buffer = dictionary[frozen_symbol]  #aa
 
@@ -163,12 +253,16 @@ def decode(data, dictionary, file, p, static_dictionary, rc):
 
         if result:
             write_file.write(result)
+        print("tamanho: ", len(dictionary))
 
 def lzw_decompress(data, file, p, static_dictionary=False, rc=False):
     dictionary = initialize_dictionary_decode()
     decode(data, dictionary, file, p, static_dictionary, rc)
 
+#p = 2 ** 12
 p = 2 ** 15
+#p = 2 ** 18
+#p = 2 ** 21
 
 file = sys.argv[1]
 fin = open(file, "rb")
@@ -182,7 +276,7 @@ fin.close()
 filename = f"compressed_{filename}.bin"  # Substitua "seu_arquivo.bin" pelo nome do seu arquivo binário
 
 inicio = time.time()
-lzw_compress(data=original_data, file=filename, p=p, static_dictionary=False, rc=False )
+lzw_compress(data=original_data, file=filename, p=p, static_dictionary=False, rc=True )
 fim = time.time()
 print("Tempo de Compressão: ", fim - inicio)
 print("Dados Comprimidos")
@@ -197,7 +291,7 @@ with open(filename, 'rb') as file:
     #print("Dados do arquivo comprimido: ", compressed_data)
 
 inicio = time.time()
-lzw_decompress(data=compressed_data, file="decompressed_"+original_filename, p=p, static_dictionary=False, rc=False)
+lzw_decompress(data=compressed_data, file="decompressed_"+original_filename, p=p, static_dictionary=False, rc=True)
 fim = time.time()
 print("Tempo de Descompressão: ", fim - inicio)
 #print("Dados descomprimidos: ", decompressed_data)
